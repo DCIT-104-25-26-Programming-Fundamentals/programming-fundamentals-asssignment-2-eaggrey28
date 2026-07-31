cfaf9220fb80623356f9d7ee113840b40dc1522a
eaggrey28 <eaggrey024@st.ug.edu.gh>
2026-07-31 21:27:42 +0000

    Implement prime number checker

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -37,30 +37,31 @@
 #include <iostream>
 using namespace std;
 
+bool isPrime(int number) {
+    if (number < 2) {
+        return false;
+    }
+
+    for (int divisor = 2; divisor * divisor <= number; divisor++) {
+        if (number % divisor == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int main() {
     int number;
-    bool isPrime = true;
 
     cout << "Enter a number: ";
     cin >> number;
 
-    if (number <= 1) {
-        isPrime = false;
-    } else {
-        for (int i = 2; i * i <= number; i++) {
-            if (number % i == 0) {
-                isPrime = false;
-                break;
-            }
-        }
-    }
-
-    if (isPrime) {
+    if (isPrime(number)) {
         cout << number << " is a prime number." << endl;
     } else {
-        cout << number << " is not a prime number." << endl;
+        cout << number << " is NOT a prime number." << endl;
     }
 
     return 0;
 }
-
